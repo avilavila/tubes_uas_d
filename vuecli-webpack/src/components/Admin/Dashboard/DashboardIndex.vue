27a9158159aa879cf19fdf2954853a3b0aa02cfc
avilavila <avilaaurelia2@gmail.com>
2022-12-21 04:34:44 +0700

    login admin

---
+++ vuecli-webpack/src/components/Admin/Dashboard/DashboardIndex.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-main>
+    <v-carousel height="90vh" dark show-arrows-on-hover draggable="true" hide-delimiter-background class="text-center mb-5">
+      <v-carousel-item v-for="(data, carindex) in data" :key="carindex" :src="data.src" class="gradient-fill">
+        <v-container fill-height>
+          <div style="max-width: 1200px">
+            <div class="text-md-h2 mb-3 text-sm-h3 text-h5 font-weight-black green--text text-center">
+              {{ data.heading.toUpperCase() }}
+            </div>
+            <p class="mb-5 green --text">{{ data.subHeading }}</p>
+          </div>
+        </v-container>
+      </v-carousel-item>
+    </v-carousel>
+    <v-item-group>
+      <v-container>
+        <v-row>
+          <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+            <v-card outlined color="green" prominent>
+              <v-img height="250" :src="card.src" class="white--text align-end"> <v-card-title v-text="card.title" class="green lighten-1 bold--text"></v-card-title></v-img>
+
+              <v-card-text class="white green--text">
+                <v-card-title><strong> MAROL </strong></v-card-title>
+                <div class="my-4 text-subtitle-1">Belanja hemat di MAROL</div>
+              </v-card-text>
+              <v-card-actions class="white">
+                <v-btn color="white" class="green ms-auto" @click="reserve">Update </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-item-group>
+  </v-main>
+</template>
+<style>
+.text {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+}
+
+.gradient-fill .v-responsive__content {
+  background: linear-gradient(to left, rgba(3, 12, 41, 0.8), rgba(5, 11, 31, 0.65));
+}
+.carousel-item img {
+  height: 100vh !important;
+}
+</style>
+<script>
+export default {
+  data: () => ({
+    cards: [
+      { title: "Oreo", src: "https://cdn-asset.jawapos.com/wp-content/uploads/2022/12/oreo-chandra.jpg", flex: 3 },
+      { title: "Ultra Milk", src: "https://cf.shopee.co.id/file/8d89f6a1bc98492d499caa1d5f08dc1c", flex: 3 },
+      { title: "Lemonilo", src: "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/1/28/7e02dc36-2047-434b-92a9-60047544491a.jpg", flex: 3 },
+      { title: "Indomie", src: "https://awsimages.detik.net.id/community/media/visual/2019/11/11/8b3c99cb-6527-4163-99f9-f6fbce370511_169.jpeg?w=700&q=90", flex: 3 },
+    ],
+    data: [
+      {
+        src: "https://image.freepik.com/free-vector/supermarket_182089-133.jpg",
+        heading: "PRODUK",
+        subHeading: "Belanja apa saja dengan harga terjamin? Hanya di MAROL!",
+      },
+      {
+        src: "https://pa-pagaralam.go.id/images/demo/anoun.png",
+        heading: "ANNOUNCEMENT",
+        subHeading: "Yuk lihat info terupdate disini.",
+      },
+      
+    ],
+  }),
+
+  methods: {
+    reserve() {
+      this.$router.push({
+        name: "Produk",
+      });
+    },
+  },
+};
+</script>
---
+++ vuecli-webpack/src/components/Admin/Dashboard/DashboardLayout.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="dashboard">
+      <v-navigation-drawer class="fullheight" width="256" v-model="drawer" absolute temporary color="grey lighten-5">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">SUPERMARKET</v-list-item-title>
+            <v-list-item-subtitle>Kelompok D</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+  
+        <v-divider></v-divider>
+  
+        <v-list dense nav>
+          <v-list-item v-for="item in items" :key="item.title" link color="light-green darken-4" tag="router-link" :to="item.to">
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar color="green lighten-3" dark>
+        <v-app-bar-nav-icon @click="drawer = true" color="white"></v-app-bar-nav-icon>
+        <VSpacer />
+
+        <v-toolbar-items>
+        <v-btn text router @click="logout()" color="white"><v-icon>mdi-power</v-icon></v-btn>
+      </v-toolbar-items>
+
+      </v-app-bar>
+      <div class="fullheight pa-5">
+        <router-view></router-view>
+      </div>
+    </div>
+  </template>
+
+<script>
+  export default {
+    name: "DashboardLayoutAdmin",
+    data() {
+      return {
+        users: [],
+        drawer: false,
+        items: [
+          { title: "Dashboard Admin", to: "/dashboardindexAdmin" },
+          // { title: "Produk", to: "/produk" },
+          { title: "Announcement Admin", to: "/announcementAdmin" },
+        ],
+      };
+    },
+    methods: {
+    logout() {
+      localStorage.removeItem("id");
+      localStorage.removeItem("token");
+      //location.reload();
+      this.$router.push({
+        name: "HomeDashboard",
+      });
+    },
+    readData() {
+      var url = this.$api + "/getuser/" + localStorage.getItem("id");
+      this.$http
+        .get(url, {
+          headers: {
+            Authorization: "Bearer" + localStorage.getItem("token"),
+          },
+        })
+        .then((response) => {
+          this.users = response.data.data;
+        });
+    },
+  },
+
+  mounted() {
+    this.readData();
+  },
+};
+</script>
+  <style scoped>
+  .fullheight {
+    min-height: 100vh !important;
+  }
+  </style>
